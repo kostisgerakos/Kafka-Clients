324971198bbe5047d8810969376c1cf910c9a0ce
kostisgerakos <kostisgerakos@gmail.com>
2018-02-13 11:38:04 +0200

    Update Consumer.java

--- src/main/java/eu/rawfie/kafka/Consumer.java
+++ src/main/java/eu/rawfie/kafka/Consumer.java
@@ -14,15 +14,15 @@ import eu.rawfie.uxv.commands.Goto;
 
 public class Consumer {
 	public static void main(String[] args) {
-		final String topic = "hmod_Goto";
+		final String topic = "Goto";
 		final Properties props = new Properties();
 		//Basic properties values
-		props.put("bootstrap.servers", "eagle5.di.uoa.gr:9092");
+		props.put("bootstrap.servers", "localhost:9092");
 		props.put("enable.auto.commit", "false");
 		props.put("group.id", "LocationConsumer");
 		props.put("key.deserializer", "io.confluent.kafka.serializers.KafkaAvroDeserializer");
 		props.put("value.deserializer", "io.confluent.kafka.serializers.KafkaAvroDeserializer");
-		props.put("schema.registry.url", "http://172.19.0.18:8081");
+		props.put("schema.registry.url", "localhost:8081");
 		props.put("specific.avro.reader", "true");
 
 		final KafkaConsumer<String, Goto> consumer = new KafkaConsumer<>(props);
