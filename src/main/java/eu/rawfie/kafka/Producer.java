e2321cb04f63d44876a72bb2b23dc674b8104c5e
kostisgerakos <kostisgerakos@gmail.com>
2018-02-13 11:39:35 +0200

    Update Producer.java

--- src/main/java/eu/rawfie/kafka/Producer.java
+++ src/main/java/eu/rawfie/kafka/Producer.java
@@ -17,17 +17,17 @@ public class Producer {
 
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
-		final String topic = "hmod_Goto";
+		final String topic = "Goto";
 		final Properties props = new Properties();
 
-		props.put("bootstrap.servers", "eagle5.di.uoa.gr:9092");
+		props.put("bootstrap.servers", "localhost:9092");
 		props.put("key.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer");
 		props.put("value.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer");
 
-		props.put("schema.registry.url", "http://eagle5.di.uoa.gr:8081");
+		props.put("schema.registry.url", "localhost:8081");
 
 		//GoTo
-		final Goto go = new Goto(new Header("uoa.kg.register","navigation",(long) 1000000000),new Location(0.0, 0.0, 0.0f, 0.0, 0.0, 0.0, 0.0f, 0.0f), 0.0f, 0.0f);
+		final Goto go = new Goto(new Header("uregister","nav",(long) 1000000000),new Location(0.0, 0.0, 0.0f, 0.0, 0.0, 0.0, 0.0f, 0.0f), 0.0f, 0.0f);
 		final ProducerRecord<String, Goto> record = new ProducerRecord<String, Goto>(topic, go);
 		final KafkaProducer<String, Goto > producer = new KafkaProducer<>(props);
 
